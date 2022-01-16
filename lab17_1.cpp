9b2bb2703dda9d576bd54d0297e0059637d5c22a
tahhh <thadpongsermsook1@gmail.com>
2022-01-16 14:10:57 +0700

    Lab17_1 completed

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,42 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = (rand()%101)/100.0;
+    }
+}
+
+void findRowSum(const double *a, double *b,int c,int d){
+    for(int j = 0; j < c; j++){
+        double l = 0;
+        for(int k = 0; k < d; k++){
+            l += *((a+k)+(d*j));
+        }
+        b[j] = l ;
+    }
+}
+
+void findColSum(const double *a, double *b, int c, int d){
+	int i = 0, j = 0;
+	b[j] = 0;
+	for(int r = 0; r < d; r++){
+		i = r;
+		for(int k = 0; k < c; k++){
+			b[j] += a[i];
+			i += d;
+		}
+		j++;
+		b[j] = 0;
+	}
+}
